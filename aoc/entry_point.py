04ba8568d621c05b2e90c334cf6bd8e4765143ee
dequeues <dequeues@outlook.com>
2021-12-06 13:24:15 +0000

    Also create test data

--- aoc/entry_point.py
+++ aoc/entry_point.py
@@ -35,6 +35,7 @@ def new(day: int, year: int) -> None:
 
     file_path_script = pathlib.Path(f"aoc/{year}/day_{day}_{year}.py")
     file_path_input = pathlib.Path(f"aoc/{year}/input_data/day_{day}")
+    file_path_test_input = pathlib.Path(f"aoc/{year}/test_data/day_{day}")
 
     try:
         with open(file_path_script, "x", encoding="utf-8") as file:
@@ -42,10 +43,11 @@ def new(day: int, year: int) -> None:
     except FileExistsError as error:
         print(f"Could not create {file_path_script}: {error}")
 
-    try:
-        with open(file_path_input, "x", encoding="utf-8"):
-            utime(file_path_input, None)
-    except FileExistsError as error:
-        print(f"Could not create {file_path_input}: {error}")
+    for f_p in [file_path_input, file_path_test_input]:
+        try:
+            with open(f_p, "x", encoding="utf-8"):
+                utime(f_p, None)
+        except FileExistsError as error:
+            print(f"Could not create {f_p}: {error}")
 
     click.echo(f"Completed processing for day {day}")
