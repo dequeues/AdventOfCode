7d73cc4dcefd3e715d1efd50a982b214ca9b5d2c
dequeues <dequeues@outlook.com>
2022-12-03 12:46:50 +0000

    Day 2, part 2

--- aoc/2022/day_02_2022.py
+++ aoc/2022/day_02_2022.py
@@ -31,8 +31,8 @@ def get_turn(turn_char: str) -> str:
 @utils.time_func
 def part1() -> int:
     my_score: int = 0
-    for turn in inputdata:
-        opponent_turn, my_turn = map(get_turn, turn.split(" "))
+    for game_round in inputdata:
+        opponent_turn, my_turn = map(get_turn, game_round.split(" "))
 
         my_score += TypeScores[my_turn].value
         if opponent_turn == my_turn:
@@ -46,7 +46,26 @@ def part1() -> int:
 
 @utils.time_func
 def part2() -> int:
-    return 0
+    my_score: int = 0
+    desired_outcomes_str = {"X": RPSRoundScoring.LOSS,
+                            "Y": RPSRoundScoring.DRAW, "Z": RPSRoundScoring.WIN}
+    for game_round in inputdata:
+        opponent_turns = game_round.split(" ")
+        opponent_turn, desired_outcome = get_turn(
+            opponent_turns[0]), desired_outcomes_str[opponent_turns[1]]
+
+        my_score += desired_outcome.value
+        if desired_outcome == RPSRoundScoring.DRAW:
+            my_score += TypeScores[opponent_turn].value
+
+        elif desired_outcome == RPSRoundScoring.LOSS:
+            my_score += TypeScores[TURN_OPTIONS[TURN_OPTIONS.index(
+                opponent_turn) - 1]].value
+        else:
+            my_score += TypeScores[TURN_OPTIONS[(TURN_OPTIONS.index(
+                opponent_turn) + 1) % 3]].value
+
+    return my_score
 
 
 utils.print_result(F'Part 1 answer: {part1()}')
