9e4d6c6126ff4eb211d204122397b1c923d1d279
dequeues <dequeues@outlook.com>
2022-12-03 11:11:37 +0000

    Day 1 part 2

--- aoc/2022/day_01_2022.py
+++ aoc/2022/day_01_2022.py
@@ -3,20 +3,21 @@ from aoc import utils
 
 inputdata = utils.get_day_data(1, test_data=False, split_delimiter="\n\n")
 
+elves: list[int] = []
+for elf in inputdata:
+    elf_values_split = elf.split("\n")
+    elves.append(sum(map(int, elf_values_split)))
+
 
 @utils.time_func
 def part1() -> int:
-    elves: list[int] = []
-    for elf in inputdata:
-        elf_values_split = elf.split("\n")
-        elves.append(sum(map(int, elf_values_split)))
-
     return max(elves)
 
 
 @utils.time_func
 def part2() -> int:
-    return 0
+    elves_sorted = sorted(elves)
+    return sum(elves_sorted[-3:])
 
 
 utils.print_result(F'Part 1 answer: {part1()}')
