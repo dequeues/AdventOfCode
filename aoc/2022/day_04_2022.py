33f12a76e35308474d62a2d59330aa52167efda4
dequeues <dequeues@outlook.com>
2022-12-04 12:03:34 +0000

    Day 4, part 2

--- aoc/2022/day_04_2022.py
+++ aoc/2022/day_04_2022.py
@@ -1,8 +1,16 @@
+from dataclasses import dataclass
 from re import findall
 from typing import Any
 from aoc import utils
 
+
 GroupPairSetType = list[list[set[int]]]
+
+
+@dataclass
+class ReturnType:
+    group_matches: int
+    overlapping_matches: int
 
 
 def setup() -> GroupPairSetType:
@@ -16,27 +24,38 @@ def setup() -> GroupPairSetType:
     return group_pairs
 
 
-@utils.time_func
-def part1(*args: Any, **kwargs: Any) -> int:  # pylint: disable=unused-argument
+def get_matches() -> ReturnType:
     data = setup()
     group_matches: int = 0
+    overlapping_matches: int = 0
 
     for group_one, group_two in data:
         if any([group_two.issubset(group_one), group_one.issubset(group_two)]):
             group_matches += 1
 
-    return group_matches
+        if group_one.intersection(group_two):
+            overlapping_matches += 1
+
+    return ReturnType(
+        group_matches=group_matches,
+        overlapping_matches=overlapping_matches
+    )
+
+
+@utils.time_func
+def part1(*args: Any, **kwargs: Any) -> int:  # pylint: disable=unused-argument
+    return get_matches().group_matches
 
 
 @utils.time_func
 def part2(*args: Any, **kwargs: Any) -> int:  # pylint: disable=unused-argument
-    return 0
+    return get_matches().overlapping_matches
 
 
 # Fixed test data
 inputdata = utils.get_day_data(4, test_data=True)
 assert part1(silent=True) == 2  # type: ignore
-assert part2(silent=True) == 0  # type: ignore
+assert part2(silent=True) == 4  # type: ignore
 
 inputdata = utils.get_day_data(4, test_data=False)
 utils.print_result(F'Part 1 answer: {part1()}')
