4637f6f7b0bf01b454d652788969ce7d113c70c0
dequeues <dequeues@outlook.com>
2022-12-07 20:03:44 +0000

    Day 7, part 1

--- README.md
+++ README.md
@@ -35,6 +35,7 @@ $ poetry run aoc new 2
 | [04](https://adventofcode.com/2022/day/4) | Camp Cleanup            | [aoc/2022/day\_04\_2022.py](/aoc/2022/day_04_2022.py) |  ⭐⭐   |
 | [05](https://adventofcode.com/2022/day/5) | Supply Stacks           | [aoc/2022/day\_05\_2022.py](/aoc/2022/day_05_2022.py) |  ⭐⭐   |
 | [06](https://adventofcode.com/2022/day/6) | Tuning Trouble          | [aoc/2022/day\_06\_2022.py](/aoc/2022/day_06_2022.py) |  ⭐⭐   |
+| [07](https://adventofcode.com/2022/day/7) | No Space Left On Device | [aoc/2022/day\_07\_2022.py](/aoc/2022/day_07_2022.py) |  ⭐   |
 
 ## 2021
 
---
+++ aoc/2022/day_07_2022.py
@@ -0,0 +1,111 @@
+from dataclasses import dataclass, field
+from typing import Any, Union, Iterator
+from aoc import utils
+
+
+@dataclass
+class File:
+    name: str
+    size: int
+
+
+@dataclass
+class Directory:
+    name: str
+    parent: Union[None, "Directory"] = None
+    size: int = 0
+    children_nodes: list[Union[File, "Directory"]] = field(default_factory=list)
+
+    def get_directory(self, directory_name: str) -> "Directory":
+        if self.name == directory_name and directory_name == "/":
+            return self
+
+        if directory_name == "..":
+            if self.parent:
+                return self.parent
+            return self
+
+        for node in self.children_nodes:
+            if isinstance(node, Directory) and node.name == directory_name:
+                return node
+        raise AttributeError(
+            (
+                f"{directory_name} not found "
+                f"(parent: {self.parent.name if self.parent else 'none'})"
+            )
+        )
+
+    def __iter__(self) -> Iterator[Union[File, "Directory"]]:
+        return iter(self.children_nodes)
+
+
+def get_sum_of_directories_with_max_size(tree: Directory, max_size: int) -> int:
+    total = 0
+    for directory in tree.children_nodes:
+        if isinstance(directory, Directory):
+            if directory.size < max_size:
+                total += directory.size
+            if len(directory.children_nodes) > 0:
+                total += get_sum_of_directories_with_max_size(directory, max_size)
+    return total
+
+
+def create_directory_tree() -> Directory:
+    directory = Directory(name="/", children_nodes=[])
+
+    current_command_group: Union[None, str] = None
+    current_directory = directory
+    for line in inputdata:
+        if line[0] == "$":
+            line = line[2:]
+            command_parts = line.split(" ")
+            current_command_group = line
+            if command_parts[0] == "cd":
+                current_directory = current_directory.get_directory(
+                    command_parts[1])
+                continue
+            if current_command_group == "ls":
+                continue
+
+        command, arg = line.split(" ")
+
+        if current_command_group == "ls":
+            if command == "dir":
+                current_directory.children_nodes.append(
+                    Directory(
+                        name=arg,
+                        children_nodes=[],
+                        parent=current_directory
+                    )
+                )
+            else:
+                current_directory.size += int(command)
+                cur = current_directory
+                while cur.parent:
+                    cur.parent.size += int(command)
+                    cur = cur.parent
+                current_directory.children_nodes.append(
+                    File(name=arg, size=int(command)))
+            continue
+    return directory
+
+
+@ utils.time_func
+def part1(*args: Any, **kwargs: Any) -> int:  # pylint: disable=unused-argument
+    directory = create_directory_tree()
+    return get_sum_of_directories_with_max_size(directory, 100000)
+
+
+@ utils.time_func
+def part2(*args: Any, **kwargs: Any) -> int:  # pylint: disable=unused-argument
+    return 0
+
+
+# Fixed test data
+inputdata = utils.get_day_data(7, test_data=True)
+assert part1(silent=True) == 95437  # type: ignore
+assert part2(silent=True) == 0  # type: ignore
+
+inputdata = utils.get_day_data(7, test_data=False)
+utils.print_result(F'Part 1 answer: {part1()}')
+utils.print_result(F'Part 2 answer: {part2()}')
